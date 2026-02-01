7ba0408cbf7581dea8adc7a2702728b6c93cf9f1
Pratik Rajendra Agale <pratikrajendraagale@Pratiks-MacBook-Air.local>
2026-02-01 14:45:05 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,20 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello from the Dashboard!")
+//This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName:'Pratik',lastName:"Agale"})
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the Hello Hello!")
+app.post("/user",(req,res)=>{
+    // saving datab to DB
+    res.send("Data Successfully saved to the database!")
 })
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the servers!")
+app.delete("/user",(req,res)=>{
+    // saving datab to DB
+    res.send("Deleted Successfully!")
 })
 
+app.use("/hello", (req, res) => {
+  res.send("Hello from the Hello Hello!");
+});
 
+//THIS will match all the HTTP method API calls to test
+app.use("/test", (req, res) => {
+  res.send("Hello from the servers!");
+});
 
-app.listen(7777,()=>{
-    console.log("Server is Sucessfully listening on port 7777")
-})
+app.use("/", (req, res) => {
+  res.send("Hello from the Dashboard!");
+});
+
+app.listen(7777, () => {
+  console.log("Server is Sucessfully listening on port 7777");
+});
