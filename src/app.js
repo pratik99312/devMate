b5739feedc34ffc8b8f7ffee1deba9c9e795a8d7
Pratik Rajendra Agale <pratikrajendraagale@Pratiks-MacBook-Air.local>
2026-02-01 15:01:35 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,33 +2,12 @@ const express = require("express");
 
 const app = express();
 
-//This will only handle GET call to /user
-app.get("/user",(req,res)=>{
-    res.send({firstName:'Pratik',lastName:"Agale"})
+app.get("/user/:userID/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({firstName:"Pratik",lastName:"Agale"})
 })
 
-app.post("/user",(req,res)=>{
-    // saving datab to DB
-    res.send("Data Successfully saved to the database!")
-})
 
-app.delete("/user",(req,res)=>{
-    // saving datab to DB
-    res.send("Deleted Successfully!")
-})
-
-app.use("/hello", (req, res) => {
-  res.send("Hello from the Hello Hello!");
-});
-
-//THIS will match all the HTTP method API calls to test
-app.use("/test", (req, res) => {
-  res.send("Hello from the servers!");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello from the Dashboard!");
-});
 
 app.listen(7777, () => {
   console.log("Server is Sucessfully listening on port 7777");
