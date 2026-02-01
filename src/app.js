84a6cc86077eb21ae7219cd5a359415f462a759a
Pratik Rajendra Agale <pratikrajendraagale@Pratiks-MacBook-Air.local>
2026-02-01 15:40:53 +0530

    Add Mutliple route handlers

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userID/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstName:"Pratik",lastName:"Agale"})
-})
+// app.use("/route",rH,[rH2,rH3],rH4)
 
-
+app.use("/user", [
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    next();
+    // res.send("Response!!");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 3!!");
+    res.send("3rd Response!!");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4!!");
+    res.send("4th Response!!");
+  },
+]);
 
 app.listen(7777, () => {
   console.log("Server is Sucessfully listening on port 7777");
