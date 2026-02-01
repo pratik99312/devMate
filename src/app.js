6af0f666632920eac70bbe0c7a4d53dd7163991b
Pratik Rajendra Agale <pratikrajendraagale@Pratiks-MacBook-Air.local>
2026-02-01 20:24:13 +0530

    Auth middleware

--- src/app.js
+++ src/app.js
@@ -2,28 +2,30 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/route",rH,[rH2,rH3],rH4)
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/user", [
-  (req, res, next) => {
-    console.log("Handling the route user!!");
-    next();
-    // res.send("Response!!");
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 2!!");
-    // res.send("2nd Response!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 3!!");
-    res.send("3rd Response!!");
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 4!!");
-    res.send("4th Response!!");
-  },
-]);
+app.use("/admin", adminAuth);
+
+app.post("/user/login",(req,res)=>{
+    res.send("User Logged in succesfully!")
+})
+
+app.get("/user", userAuth, (req, res) => {
+  // Logic of checking if the request is authorised
+  res.send("All Data Sent to user");
+});
+
+
+
+app.get("/admin/getAllData", (req, res) => {
+  // Logic of checking if the request is authorised
+  res.send("All Data Sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  // Logic of fetching all data
+  res.send("User Deleted");
+});
 
 app.listen(7777, () => {
   console.log("Server is Sucessfully listening on port 7777");
