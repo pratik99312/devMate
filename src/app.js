867e884a88686736ab38b93584a4e1c707320989
Pratik Rajendra Agale <pratikrajendraagale@Pratiks-MacBook-Air.local>
2026-02-08 10:09:32 +0530

    Diving into APIs

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.post("/signup", async (req, res) => {
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   try {
-    console.log(userEmail)
+    console.log(userEmail);
     const user = await User.findOne({ emailId: userEmail });
     res.send(user);
     // if (users.length === 0) {
@@ -40,6 +40,30 @@ app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
     res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Delete API - delete by ID in users from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ userId }); // same as down --> shorthand
+    // const user = await User.findByIdAndDelete({ _id: userId });
+    res.send("User deleted Sucessfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// UPDATE data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data,{returnDocument:'before'});
+    res.send("User Updated Successfully");
   } catch (error) {
     res.status(400).send("Something went wrong");
   }
